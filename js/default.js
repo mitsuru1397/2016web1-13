91c1a6cc4c7d863eab488234d732ea6c97309565
mitsuru1397 <g1527068@st.t-kougei.ac.jp>
2016-07-04 18:19:24 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -27,6 +27,15 @@ var refresh = function(){
 }
 update();
 
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+ opt = document.querySelector('option[value="other.html"]');
+}else{
+ opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected = true;
+
 document.getElementById('form').select.onchange = function(){
  location.href = document.getElementById('form').select.value;
 }
