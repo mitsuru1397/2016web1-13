f7a38a09a812cc173a80b255393176e7e30401ce
mitsuru1397 <g1527068@st.t-kougei.ac.jp>
2016-07-04 17:29:45 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -10,17 +10,17 @@ var separate_time = function(time){
  
  
 var update = function(){ 
-var now = new Date();
-var counter = separate_time(now);
-document.getElementById('countdown').textContent =
- counter[5] + '年' +
- counter[4] + '月' +
- counter[3] + '日' +
- counter[2] + '時' +
- counter[1] + '分' +
- counter[0] + '秒';
+ var now = new Date();
+ var counter = separate_time(now);
+ document.getElementById('countdown').textContent =
+  counter[5] + '年' +
+  counter[4] + '月' +
+  counter[3] + '日' +
+  counter[2] + '時' +
+  counter[1] + '分' +
+  counter[0] + '秒';
  
- refrefh();
+  refresh();
 }
 
 var refresh = function(){
